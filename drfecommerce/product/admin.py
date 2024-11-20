f38b01f54a035adb35b077d914d3e2aff1ec805a
yahia <yahiaeltaralm@gmail.com>
2024-11-20 17:40:06 +0200

    admin pannel customization

--- drfecommerce/product/admin.py
+++ drfecommerce/product/admin.py
@@ -2,7 +2,25 @@ from django.contrib import admin
 from .models import Category, Brand, Product
 
 
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'parent']
+    list_filter = ['name', 'parent']
+    search_fields = ['name']
+
+
+class BrandgAdmin(admin.ModelAdmin):
+    list_display = ['name', 'id']
+    list_filter = ['name']
+    search_fields = ['name']
+
+
+class ProductgAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'is_digital', 'brand', 'category']
+    list_filter = ['name', 'is_digital', 'brand', 'category']
+    search_fields = ['name']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Brand, BrandgAdmin)
+admin.site.register(Product, ProductgAdmin)
 
