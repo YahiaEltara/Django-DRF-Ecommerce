43a2c1f9ecdf81f0704febeb1d8a381d132b1d28
yahia <yahiaeltaralm@gmail.com>
2024-11-23 08:40:17 +0200

    applying JWT Authentication

--- drfecommerce/urls.py
+++ drfecommerce/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from drfecommerce.product import views
 from rest_framework.routers import DefaultRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register('category', views.CategoryViewSet)
@@ -20,6 +20,10 @@ urlpatterns = [
 
     # ViewSets
     path('api/', include(router.urls)),
+
+    # JWT Authentication
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
 
